5c2a4355997216dd276679325c16a8f2edc602bf
Sivakumar Kailasam <sivakumar.ur.friend@gmail.com>
2016-03-10 18:04:52 +0530

    port change

--- code/app.py
+++ code/app.py
@@ -9,5 +9,5 @@ app = Flask(__name__)
 def hello():
     return "Of course it works! 🐍 "
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
